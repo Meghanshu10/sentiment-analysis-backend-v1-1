5d9cf0daa512bd8554bf90f2ebcd814308bccfd6
Meghanshu <meghanshu.bansal14@gmail.com>
2022-02-15 13:06:25 +0530

    port added succesfully

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const needle = require('needle');
 const express = require('express')
 const bodyParser = require('body-parser');
 const { query } = require('express');
+const port = process.env.PORT || 3000
 
 // The code below sets the bearer token from your environment variables
 // To set environment variables on macOS or Linux, run the export command below from the terminal:
